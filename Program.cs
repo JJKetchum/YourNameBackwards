972ca8abbb2d6bb75d26d1103fc4dfa760f82662
JJKet <JJKet@EPIC-COMPUTER>
2024-02-14 13:21:15 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.InteropServices;
+
+string Name;
+Console.WriteLine("What is your name?");
+Name = Console.ReadLine();
+string[] letters = new string[Name.Length];
+
+for (int i = 0; i < Name.Length; i++)
+{
+    letters[i] = Name[i].ToString();
+}
+
+string[] backletters = new string[letters.Length];
+
+for (int e = 0; e < letters.Length; e++)
+{
+    backletters[e] = letters[(letters.Length - 1) - e];
+}
+
+string backname = "";
+
+for (int c = 0; c < letters.Length; c++)
+{
+    backname += backletters[c];
+}
+
+Console.WriteLine("Your name backwards is: " + backname);
+
